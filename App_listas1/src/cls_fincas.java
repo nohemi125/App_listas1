f613f4acc8a26c53af8b0498f5dd6239430bc7dc
nohemi125 <nohemy_morelo69222@elpoli.edu.co>
2023-10-14 16:35:11 -0500

    commit3
    funcion limpiar
    funcion guardar

--- App_listas1/src/cls_fincas.java
+++ App_listas1/src/cls_fincas.java
@@ -7,7 +7,24 @@ public class cls_fincas {
     private String str_propietario;
     
     public cls_fincas (String n, String c, String d,String co, String p){
-        
+        this.str_nombre = n; //no se trabaja con los parametros , se trabaja es con los atributos
+        this.str_codigo = c;
+        this.str_direccion = d;
+        this:str_contacto = co;
+        this.str_propietario = p;
     }
-            
+    //setters
+        public void setNombre(String nombre){this.str_nombre = nombre;}  
+        public void setDireccion (String direccion){this.str_direccion = direccion;}
+        public void setContacto(String contacto){this.str_contacto = contacto;}
+        public void setPropietario (String propietario){this.str_propietario = propietario;}
+        
+        //getters los geeters son retornables
+        
+        public String  getNombre(){return this.str_nombre;}
+        public String getCodigo(){return this.str_codigo;}
+        public String getDireccion(){ return this.str_direccion;}
+        public String getContacto(){ return this.str_contacto;}
+        public String getPropietario(){ return this.str_propietario;}
+        
 }
