6c67ec41a2705569a2bcd1664dee216061e2b993
nohemi125 <nohemy_morelo69222@elpoli.edu.co>
2023-10-14 15:01:58 -0500

    commit1
    creacion del proyecto
    creacion de reporcitorio
    creacion de la clases

---
+++ App_listas1/src/cls_fincas.java
@@ -0,0 +1,13 @@
+
+public class cls_fincas {
+    private String str_nombre;
+    private String str_codigo;
+    private String str_direccion;
+    private String str_contacto;
+    private String str_propietario;
+    
+    public cls_fincas (String n, String c, String d,String co, String p){
+        
+    }
+            
+}
